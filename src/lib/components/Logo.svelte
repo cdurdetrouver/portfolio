c21a4c841f13792b384c87c810e347dc2223b7cb
cdurdetrouver <gabriel.bazart@gmail.com>
2025-03-02 16:23:50 +0100

    feat: setup finish

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,4 @@
+<a href="/" class="flex gap-[10px] items-center">
+	<img src="little-logo.png" alt="logo" class="w-[50px]"/>
+	<strong class="text-xl uppercase hidden md:block select-none">Cdurdetrouver</strong>
+</a>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,9 @@
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
+	// Components
+	import Logo from '$lib/components/Logo.svelte';
+
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
@@ -29,28 +32,12 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">Skeleton</strong>
+				<Logo />
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
+					href="https://github.com/cdurdetrouver"
 					target="_blank"
 					rel="noreferrer"
 				>
